a3ed44911300d9a51d6657c1a872e03b6a8b0505
Tkisiga <ktimo550@gmail.com>
2020-03-13 12:43:22 +0300

    connected postgreSQL database called IMDS_DB

--- Project11/Project11/settings.py
+++ Project11/Project11/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Project11.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'IMDS_DB',
+        'USER':'postgres',
+        'PASSWORD':'devereuxsmiles',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
