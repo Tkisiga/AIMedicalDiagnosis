f6abbcd56d6d4aea38be4e194b9793ca0e3805ac
Tkisiga <ktimo550@gmail.com>
2020-04-03 16:03:50 +0300

    boon

--- Project11/IMDS/models.py
+++ Project11/IMDS/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Patients(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    Name=models.TextField(max_length=255,null=False,editable=True)
+    Age=models.IntegerField(null=False,editable=False)
+    Gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    DOB = models.DateField()
+
+   
+
